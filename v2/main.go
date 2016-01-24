15efd5856209ae588e541dd1a551140101f99da9
Kelsey Hightower <kelsey.hightower@gmail.com>
2016-01-23 21:36:26 -0800

    add helloworld app

---
+++ v2/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		userAgent := r.Header.Get("User-Agent")
+		log.Printf("%s - [%s %s %s] 200 [%s]", r.RemoteAddr, r.Method, r.URL.Path, r.Proto, userAgent)
+		w.Header().Set("Server", "Hello/v2")
+		fmt.Fprintf(w, "Hello, World!\n")
+	})
+
+	log.Println("Starting Hello service...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
